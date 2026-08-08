e40389b58ddb86ca7016778f94f1255b92da1a38
Valentina Villegas <vvillegasc@unbosque.edu.co>
2026-08-07 20:45:54 -0500

    Update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://roicort.github.io",
+  site: "https://vvillegasc.github.io",
   base: "/",
   integrations: [mdx(), sitemap(), pagefind(), icon()],
 
